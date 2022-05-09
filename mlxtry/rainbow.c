a19584c1e8a9fbd9a98a8407bd232aba35a50a08
ialina <alinapielaszkiewicz@gmail.com>
2022-05-09 10:03:09 +0200

    rainbow done

--- mlxtry/rainbow.c
+++ mlxtry/rainbow.c
@@ -38,13 +38,12 @@ int	main(void)
 
 	init_color(&color);
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 5, 1000, "try");
-	while (y <= 800)
+	win_ptr = mlx_new_window(mlx_ptr, 300, 1400, "try");
+	while (y <= 1021)
 	{
 		mlx_pixel_put(mlx_ptr, win_ptr, x, y, c);
-		printf("x = %d,    y = %d,     c = %x\n", x, y, c);
 		x++;
-		if (x == 5)
+		if (x == 300)
 		{
 			x = 0;
 			if (c == color.red)
@@ -55,31 +54,32 @@ int	main(void)
 				c = c - 0x010000; // removing red
 			else if (c > color.green && c < color.sea)
 				c = c + 0x000001; // 65281 - 65536 = -255
+			else if (c == color.sea)
+				c = c - 0x000100; // removing green
 			else if (c < color.yellow && c > color.green)
 				c = c - 0x010000;
 			else if (c == color.green)
-			{
-				printf("========before c = %x\n", c);
 				c = c + 0x000001; //adding blue
-				printf("========after c = %x\n", c);
-				printf("yo wtf!\n");
-			}
-			else if (c == color.sea)
-				c = c - 0x000100; // removing green
 			else if (c < color.sea && c > color.blue)
-			{
 				c = c - 0x000100;
-				printf("are you fck here!\n");
-		}
 			else if (c == color.blue)
+				c = c + 0x010000; // adding red
+			y++;
+		}
+	}
+	while (y > 1021 && y < 1500)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, c);
+		x++;
+		if (x == 300)
+		{
+			x = 0;
+			if (c == color.blue)
 				c = c + 0x010000; // adding red
 			else if (c > color.blue && c < color.red)
 				c = c + 0x010000;
 			y++;
-			// printf("x = %d,y = %d\n", x, y);
-			mlx_pixel_put(mlx_ptr, win_ptr, 200, 600, color.sea);
-			}
+		}
 	}
-	// mlx_pixel_put(mlx_ptr, win_ptr, 200, 200, -250);
 	mlx_loop(mlx_ptr);
 }
