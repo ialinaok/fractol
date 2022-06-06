8eed06df7c54d71d283d9f3d5dd77d4a4d21518b
ialina <alinapielaszkiewicz@gmail.com>
2022-06-06 14:10:21 +0200

    adding zoom

--- fractol.h
+++ fractol.h
@@ -12,19 +12,11 @@
 
 #define WIN_X 936	// 1300
 #define WIN_Y 864	// 1200
-#define MAX_ITER 255
-
-typedef	struct	s_opti
-{
-	int	x_min_offset;
-	int	x_max_offset;
-	int	y_min_offset;
-	int	y_max_offset;
-}				t_opti;
+#define MAX_ITER 100
 
 typedef struct	s_coordi
 {
-	float	x_min;
+	double	x_min;
 	double	x_max;
 	double	y_min;
 	double	y_max;
@@ -79,11 +71,12 @@ int		close_x(t_data *data);
 void	arrow_moves(int keycode, t_data *data);
 void	put_image_to_image(void *dest, void *src, int x_offset, int y_offset);
 /* zoom.c */
-int	mouse_zoom(int button, int x, int y, t_data	*data);
+int		mouse_zoom(int button, int x, int y, t_data	*data);
+void	calc_distance(t_complex point, t_coordi *dist, t_data *data);
 /* render.c */
-int	render(t_data *data);
-int	get_started(t_data *data);
-int	dumb_colors(void);
+int		render(t_data *data);
+int		get_started(t_data *data);
+int		dumb_colors(void);
 void	dumb_shit(t_data *data);
 /* trgb.c */
 int	create_trgb(int t, int r, int g, int b);
@@ -98,16 +91,16 @@ void	bernstein(int iterations, t_trgb *trgb);
 void	blue(int iterations, t_trgb *trgb);
 /* mandelbrot.c */
 void		mandelbrot_init(t_data *data);
-t_complex	mandelbrot_pxl_to_cmplx(t_data *data);
 int			mandelbrot_iter(t_complex *c);
 void		mandelbrot(t_data *data);
 /* julia.c */
 void	julia_init(t_data *data);
-int	normal_julia_iter(t_complex *z, t_complex c);
+int		normal_julia_iter(t_complex *z, t_complex c);
 void	normal_julia(t_data *data, double r, double i);
 /* cmplx_math.c */
 t_complex	multi_cmplx(t_complex z, t_complex c);
 t_complex	add_cmplx(t_complex z, t_complex *c);
 double		abs_of_cmplx(t_complex z);
+t_complex	pxl_to_cmplx(t_data *data, int x, int y);
 
 #endif
--- srcs/zoom.c
+++ srcs/zoom.c
@@ -6,47 +6,40 @@
 /*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:52:29 by apielasz          #+#    #+#             */
-/*   Updated: 2022/06/06 01:35:00 by apielasz         ###   ########.fr       */
+/*   Updated: 2022/06/06 14:06:23 by apielasz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
+void	calc_distance(t_complex point, t_coordi *dist, t_data *data)
+{
+	dist->x_min = point.r - data->screen.x_min;
+	dist->x_max = data->screen.x_max - point.r;
+	dist->y_min = point.i - data->screen.y_min;
+	dist->y_max = data->screen.y_max - point.i;
+}
+
 int	mouse_zoom(int button, int x, int y, t_data	*data)
 {
-	printf("%d x: %d, y: %d\n", button, x, y);
+	t_complex	point;
+	t_coordi		dist;
 
-	if (button == 1) // scroll down == zoom in == decrease the limits
+	point = pxl_to_cmplx(data, x, y);
+	calc_distance(point, &dist, data);
+	if (button == 2 || button == 5) // zoom out // right click // scroll up
 	{
-		// printf(" xmax %f --- before\n", data->screen.x_max);
-		// printf(" xmin %f --- before\n", data->screen.x_min);
-		// printf(" ymax %f --- before\n", data->screen.y_max);
-		// printf(" ymin %f --- before\n", data->screen.y_min);
-		data->screen.x_max = data->screen.x_max * 1.2;
-		data->screen.x_min = data->screen.x_min * 1.2;
-		data->screen.y_max = data->screen.y_max * 1.2;
-		data->screen.y_min = data->screen.y_min * 1.2;
-		// data->screen.x_max = x / WIN_X * (data->screen.x_max - data->screen.x_min) / 1.1;
-		// data->screen.x_min = x / WIN_X * (data->screen.x_max - data->screen.x_min) / 1.1;
-		// data->screen.x_max = x / WIN_X * (data->screen.x_max - data->screen.x_min) / 1.1;
-		// data->screen.x_max = x / WIN_X * (data->screen.x_max - data->screen.x_min) / 1.1;
-
-		// printf(" xmax %f --- scroll down\n", data->screen.x_max);
-		// printf(" xmin %f --- scroll down\n", data->screen.x_min);
-		// printf(" ymax %f --- scroll down\n", data->screen.y_max);
-		// printf(" ymin %f --- scroll down\n", data->screen.y_min);
-		// printf(" xmax %f --- scroll down\n", xmax);
-		// printf(" xmin %f --- scroll down\n", xmin);
-		// printf(" ymax %f --- scroll down\n", ymax);
-		// printf(" ymin %f --- scroll down\n", ymin);
+		data->screen.x_min = data->screen.x_min - dist.x_min * 0.1;
+		data->screen.x_max = data->screen.x_max + dist.x_max * 0.1;
+		data->screen.y_min = data->screen.y_min - dist.y_min * 0.1;
+		data->screen.y_max = data->screen.y_max + dist.y_max * 0.1;
 	}
-	if (button == 2) // scroll up == zoom out == increase the limits
+	if (button == 1 || button == 4) // zoom in // left click // scroll down
 	{
-		data->screen.x_max = data->screen.x_max * 0.8;
-		data->screen.x_min = data->screen.x_min * 0.8;
-		data->screen.y_max = data->screen.y_max * 0.8;
-		data->screen.y_min = data->screen.y_min * 0.8;
-		printf(" xmax %f === scroll up\n", data->screen.x_max);
+		data->screen.x_min = data->screen.x_min + dist.x_min * 0.1;
+		data->screen.x_max = data->screen.x_max - dist.x_max * 0.1;
+		data->screen.y_min = data->screen.y_min + dist.y_min * 0.1;
+		data->screen.y_max = data->screen.y_max - dist.y_max * 0.1;
 	}
 	render(data);
 	return (0);
